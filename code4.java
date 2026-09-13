7d6172d5c9a19dc2d56d994d29bb4f68215c5a49
apurvajain025 <apurvajain025@gmail.com>
2026-09-13 16:36:00 +0530

    added problem4

---
+++ code4.java
@@ -0,0 +1,36 @@
+class IdCard {
+    String name;
+    int booksIssued;
+
+    // Constructor
+    IdCard(String name, int booksIssued) {
+        this.name = name;
+        this.booksIssued = booksIssued;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        // First object
+        IdCard ravi = new IdCard("Ravi", 0);
+
+        // Second variable pointing to same object
+        IdCard duplicate = ravi;
+
+        // Change through second variable
+        duplicate.booksIssued = 3;
+
+        // Third separate object
+        IdCard separate = new IdCard("Ravi", 3);
+
+        System.out.println(
+            "Ravi's booksIssued (via first variable): "
+            + ravi.booksIssued
+        );
+
+        System.out.println("duplicate == ravi: " + (duplicate == ravi));
+
+        System.out.println("separate == ravi: " + (separate == ravi));
+    }
+}
